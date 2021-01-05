16cd82904222510e71da2f052b9112329d35e392
Ashley Kircher <ashleywkircher@gmail.com>
2021-01-05 15:46:56 -0600

    Beginning work on combining multiples, creating amount field, and adding a stable key to cart items

--- src/Components/CartData.js
+++ src/Components/CartData.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
+//import { Utilities } from "./Utilities";
 
 const CartData = function() {
+    //always start with what's in localStorage, and if nothing's there an empty array
     const getSavedCart = () => {
         let savedCart = JSON.parse(localStorage.getItem('cart'));
         if (!savedCart) {
@@ -8,13 +10,16 @@ const CartData = function() {
         } 
         return savedCart;
     }
-
     const [cart, setCart] = useState(getSavedCart());
     
     const addToCart = function(event) {
         event.preventDefault();
         const elem = event.target;
         const productId = elem.dataset.id;
+        //use Utilities to combine multiples and generate stable key
+        //each state array item needs an amount in addition to the product ID and stable key
+        //amount can be set by user when adding
+        //amount needs to update as more products are added
         setCart([...cart, productId]);
     }
 
---
+++ src/Components/Utilities.js
@@ -0,0 +1,13 @@
+const Utilities = function() {
+    const combineMultiples = function() {
+        //do something with filter maybe
+        console.log("I'm combining things!")
+    };
+    const addId = function() {
+        //do something
+        console.log("I'm adding an ID!")
+    }
+    return { combineMultiples, addId }
+}
+
+export default Utilities;
