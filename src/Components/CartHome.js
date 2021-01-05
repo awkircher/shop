88b3da60be8ba0ecf988a54a28576e0050db54c1
Ashley Kircher <ashleywkircher@gmail.com>
2021-01-05 15:27:46 -0600

    Local storage array of product IDs is matched with product data, and product names of what's in your cart is displayed

--- src/Components/CartHome.js
+++ src/Components/CartHome.js
@@ -1,14 +1,39 @@
 import Navigation from "./Navigation"
 import CartData from "./CartData"
+import Data from "./Data"
 
 const CartHome = function() {
-    const cartData = CartData(); //object returned contains cart state array
-    const itemsInCart = cartData.cart;
+    const cartData = CartData(); //returns object with cart property and addToCart method
+    const products = Data(); //returns the array "products" containing objects, e.g. "products[0].name"
+    const itemIdsInCart = cartData.cart; //this property contains the state array of product ID strings
+    
+    //make a new array of product objects by matching each itemId in state
+    const getMatchingProductObjects = function() {
+        const matchingProductObjects = itemIdsInCart.map((itemId) => {
+            const isMatchingId = function(item) {
+                return item.id === itemId;
+            }
+            const indexOfProductMatch = products.findIndex(isMatchingId);
+            return products[indexOfProductMatch];
+        });
+        return matchingProductObjects;
+    };
+
+    const matchingProductObjects = getMatchingProductObjects();
+    //map over the matchingProductObjects and render the product name of each
+    const cartContentsDisplay = matchingProductObjects.map((matchingProductObject, index) => {
+        return (
+            <div className="item" key={index}>
+                <h1>{matchingProductObject.name}</h1>
+            </div>
+        )
+    });
+
     return (
         <div className="CartHome">
             <Navigation />
             Your Cart
-            {itemsInCart}
+            {cartContentsDisplay}
         </div>
     );
 }
