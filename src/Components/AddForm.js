07c3a74987c7bac38e9a48fffe6b6505582c2cd5
Ashley Kircher <ashleywkircher@gmail.com>
2021-01-07 13:15:18 -0600

    Updated the AddForm to have a default value of 1

--- src/Components/AddForm.js
+++ src/Components/AddForm.js
@@ -1,7 +1,15 @@
+import { useState } from 'react'
+
 const AddForm = function(props) {
+    // Allow for a default value of "1" while keeping this a controlled component.
+    const [value, setValue] = useState("1");
+    function handleChange(event) {
+        setValue(event.target.value);
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
-        // Pull all the data off the event
+        // Pull all the data off the event.
         const elem = event.target;
         const productId = elem.dataset.id;
         const productName = elem.dataset.name;
@@ -11,10 +19,11 @@ const AddForm = function(props) {
         // Pass arguments back to CartData addToCart method. UID will be calculated there.
         props.addToCart(productId, productName, productPrice, productImg, quantity);
     }
+
     return (
         <div className="AddForm">
             <form data-id={props.id} data-name={props.name} data-price={props.price} data-img={props.img} onSubmit={handleSubmit}>
-                <input type="number"></input>
+                <input type="number" value={value} onChange={handleChange}></input>
                 <button type="submit">Add to Cart</button>
             </form>
         </div>
