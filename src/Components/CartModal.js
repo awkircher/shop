900be15087d1a55b4263864060698606f09076fa
Ashley Kircher <ashleywkircher@gmail.com>
2021-01-07 15:43:16 -0600

    Cart modal shows cart contents or empty cart message. Cart home shows empty cart message if cart is empty.

--- src/Components/CartModal.js
+++ src/Components/CartModal.js
@@ -1,8 +1,26 @@
+import CartData from '../Data/CartData'
+
 const CartModal = function(props) {
     if (props.visibility) {
+        const cartData = CartData();
+        const itemsInCart = cartData.cart;
+        let cartContents;
+        if (itemsInCart.length === 0) {
+            cartContents = "Your cart is empty."
+        } else {
+            cartContents = itemsInCart.map((item) => {
+                return (
+                    <div className="item" key={item.uid}>
+                        <h1>{item.productName}</h1>
+                        <p>{item.quantity} bags</p>
+                        <p>{item.productPrice} each</p>
+                    </div>
+                )
+            });
+        }
         return (
             <div className="CartModal">
-                Your little cart
+                {cartContents}
             </div>
         );
     } else {
--- src/Views/CartHome.js
+++ src/Views/CartHome.js
@@ -6,28 +6,31 @@ import EditItem from "../Components/EditItem"
 const CartHome = function() {
     const cartData = CartData();
     const itemsInCart = cartData.cart;
-    
-    const cartContents = itemsInCart.map((item) => {
-        return (
-            <div className="item" key={item.uid}>
-                <h1>{item.productName}</h1>
-                <p>{item.quantity} bags</p>
-                <p>{item.productPrice} each</p>
-                <RemoveItem 
-                uid={item.uid}
-                removeItem={cartData.removeItem}/>
-                <EditItem 
-                uid={item.uid}
-                value={item.quantity}
-                editItem={cartData.editItem}/>
-            </div>
-        )
-    });
+    let cartContents;
+    if (itemsInCart.length === 0) {
+        cartContents = "Your cart is empty."
+    } else {
+        cartContents = itemsInCart.map((item) => {
+            return (
+                <div className="item" key={item.uid}>
+                    <h1>{item.productName}</h1>
+                    <p>{item.quantity} bags</p>
+                    <p>{item.productPrice} each</p>
+                    <RemoveItem 
+                    uid={item.uid}
+                    removeItem={cartData.removeItem}/>
+                    <EditItem 
+                    uid={item.uid}
+                    value={item.quantity}
+                    editItem={cartData.editItem}/>
+                </div>
+            )
+        });
+    }
 
     return (
         <div className="CartHome">
             <Navigation />
-            Your Cart
             {cartContents}
         </div>
     );
