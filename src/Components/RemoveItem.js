854e58eab064866b5c9525a98fd576a9f6f8babb
Ashley Kircher <ashleywkircher@gmail.com>
2021-01-11 12:40:03 -0600

    Swapped in a remove icon for the remove item button

--- src/Components/RemoveItem.js
+++ src/Components/RemoveItem.js
@@ -1,10 +1,12 @@
+import '../App.css'
+
 const RemoveItem = function(props) {
     function handleClick() {
         props.removeItem(props.uid)
     }
     return (
         <div className="RemoveItem">
-            <button onClick={handleClick}>Remove from Cart</button>
+            <button onClick={handleClick}></button>
         </div>
     );
 }
