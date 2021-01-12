4429c64b7baafe79ed73044d2be3d8db6718011e
Ashley Kircher <ashleywkircher@gmail.com>
2021-01-12 16:49:58 -0600

    Summary shows only when you have items in the cart, display-only checkout button added, back to shopping text button added

--- src/Views/CartHome.js
+++ src/Views/CartHome.js
@@ -5,18 +5,13 @@ const CartHome = function(props) {
     const subtotal = (props.cartCombinedPrices).toFixed(2);
     const calculatedTax = ((props.cartCombinedPrices)*0.05).toFixed(2);
     const total = (props.cartCombinedPrices + Number(calculatedTax)).toFixed(2);
-    return (
-        <div className="CartHome">
-            <Navigation 
-            cartContents={props.cartContents}
-            totalQuantity={props.totalQuantity}
-            changeModalVisibility={props.changeModalVisibility}
-            modalVisibility={props.modalVisibility}
-            />
-            <div className="container">
-                <div className="items">
-                    {props.cartContents}
-                </div>
+    const getSummary = function() {
+        if (props.cartContents.length === undefined) {
+            return (
+                <div></div>
+            )
+        } else {
+            return (
                 <div className="summary">
                     <h1>Order Summary</h1>
                     <div className="lineItem">
@@ -35,7 +30,26 @@ const CartHome = function(props) {
                         <div className="label">Total</div>
                         <div className="amount">${total}</div>
                     </div>
+                    <button className="checkout">Checkout</button>
                 </div>
+            )
+        }
+    }
+    const summary = getSummary();
+    return (
+        <div className="CartHome">
+            <Navigation 
+            cartContents={props.cartContents}
+            totalQuantity={props.totalQuantity}
+            changeModalVisibility={props.changeModalVisibility}
+            modalVisibility={props.modalVisibility}
+            />
+            <div className="container">
+                <div className="items">
+                    <a className="back" href="./shop"><span>&#10158;</span> Back to shopping</a>
+                    {props.cartContents}
+                </div>
+                {summary}
             </div>
         </div>
     );
