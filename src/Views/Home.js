da813c16c1bdb0b21cbc866cb5cc8f8361eaea7c
Ashley Kircher <ashleywkircher@gmail.com>
2021-01-12 10:37:15 -0600

    homepage styling

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -6,17 +6,17 @@ function Home(props) {
   return (
     <div className="Home">
       <div className="content">
-        <h1>Welcome to Cream City</h1>
+        <h1>Welcome to Cream City.</h1>
         <p className="aboutbeans">
           Cream City Coffee Roasters crafts rich brews in Milwaukee's Riverwest neighborhood.
-          <a href="./shop">Shop our beans</a>
+          <a href="./shop">Shop our beans &#10157;</a>
         </p>
         <p className="aboutlocations">
           Founded in 2002, we've been supplying coffee shops with freshly roasted beans all over the Midwest for nearly 20 years.
-          <a href="./locations">Find brew</a>
+          <a href="./locations">Find brew &#10157;</a>
         </p>
-        <img src={hero} alt="cup of coffee with scattered beans"></img>
       </div>
+      <img className="hero" src={hero} alt="cup of coffee with scattered beans"></img>
       <Navigation 
         cartContents={props.cartContents}
         totalQuantity={props.totalQuantity}
